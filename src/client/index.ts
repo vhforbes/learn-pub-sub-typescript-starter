056da1d1a9b9300170aac07c9da23e922f1baae2
vhforbes <vhforbes@gmail.com>
2026-05-20 09:51:00 -0300

    game logic

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,18 @@
 import { connect } from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  clientWelcome,
+  commandStatus,
+  getInput,
+  printClientHelp,
+} from "../internal/gamelogic/gamelogic.js";
 import {
   declareAndBind,
   SimpleQueueType,
 } from "../internal/pubsub/declareAndBind.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   console.log("Starting Peril client...");
@@ -22,6 +30,29 @@ async function main() {
     PauseKey,
     SimpleQueueType.Transient,
   );
+
+  const game = new GameState(username);
+
+  while (true) {
+    const input = await getInput();
+
+    if (input[0] === "spawn") {
+      commandSpawn(game, input);
+    } else if (input[0] === "move") {
+      commandMove(game, input);
+    } else if (input[0] === "status") {
+      commandStatus(game);
+    } else if (input[0] === "help") {
+      printClientHelp();
+    } else if (input[0] === "spam") {
+      console.log("Spamming not allowed yet!");
+    } else if (input[0] === "quit") {
+      console.log("Exiting...");
+      break;
+    } else {
+      console.log(input[0], "is not a valid command");
+    }
+  }
 }
 
 main().catch((err) => {
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   console.log("Starting Peril server...");
@@ -16,6 +17,31 @@ async function main() {
   await publishJSON(ch, ExchangePerilDirect, PauseKey, {
     isPaused: true,
   });
+
+  printServerHelp();
+
+  while (true) {
+    const input = await getInput();
+
+    if (input[0] === "pause") {
+      console.log("sending pause message");
+
+      await publishJSON(ch, ExchangePerilDirect, PauseKey, {
+        isPaused: true,
+      });
+    } else if (input[0] === "resume") {
+      console.log("sending resume message");
+
+      await publishJSON(ch, ExchangePerilDirect, PauseKey, {
+        isPaused: false,
+      });
+    } else if (input[0] === "quit") {
+      console.log("exiting...");
+      break;
+    } else {
+      console.log(input[0], "is not a valid command");
+    }
+  }
 
   process.on("SIGINT", () => {
     conn.close();
